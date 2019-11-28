70832c92940c31f1a3d789b0f34ef17afec25bbb
Mohammed Alnadi <mohammed.alnadi@gmail.com>
2019-11-27 21:29:10 -0500

    Add Album functionality

--- app/src/main/java/com/example/photos/MainActivity.java
+++ app/src/main/java/com/example/photos/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.photos;
 
+import java.util.*;
+
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.photos.models.Photo;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static HashMap<String, HashSet<Photo>> albums = new HashMap<String, HashSet<Photo>>();
+
+    ListView albumsList;
+    Button addAlbumButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.albums_view);
+
+        addAlbumButton = findViewById(R.id.add_album);
+        albumsList=(ListView)findViewById(R.id.albums);
+
+        addAlbumButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                final View mView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+                final EditText mAlbumName = (EditText) mView.findViewById(R.id.etAlbumName);
+                Button mLogin = (Button) mView.findViewById(R.id.btnLogin);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                mLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){
+                        if(!mAlbumName.getText().toString().isEmpty() && !albums.containsKey(mAlbumName.getText().toString())){
+                            Toast.makeText(MainActivity.this, "New Album Created", Toast.LENGTH_SHORT).show();
+                            albums.put(mAlbumName.getText().toString(), new HashSet<Photo>());
+                            refreshAlbums();
+                            mView.setVisibility(View.GONE);
+                            dialog.dismiss();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error Creating Album", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        refreshAlbums();
+    }
+
+    public void refreshAlbums(){
+        List<String> listOfAlbums = new ArrayList<String>(albums.keySet());
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,listOfAlbums);
+        albumsList.setAdapter(arrayAdapter);
     }
 }
---
+++ app/src/main/java/com/example/photos/models/Photo.java
@@ -0,0 +1,123 @@
+package com.example.photos.models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * Photo object which is serialized into files that can be retrieved later
+ * @author Mohammed Alnadi
+ * @author Salman Hashmi
+ */
+public class Photo implements Serializable {
+    private String location;
+    private String caption;
+    private Map<String, Set<String>> tags;
+    private Date dateTaken;
+
+    /**
+     * Constructor
+     * @param location photo filepath
+     * @param caption photo caption
+     * @param dateTaken photo date
+     */
+    public Photo(String location, String caption, Date dateTaken) {
+        this.location = location;
+        this.caption = caption;
+        this.dateTaken = dateTaken;
+        this.tags = new HashMap<>();
+    }
+
+    /**
+     * Retrieves location
+     * @return photo filepath
+     */
+    public String getLocation() {
+        return this.location;
+    }
+
+    /**
+     * Returns caption
+     * @return photo caption
+     */
+    public String getCaption() {
+        return this.caption;
+    }
+
+    /**
+     * Returns photo tags
+     * @return photo tags
+     */
+    public Map<String, Set<String>> getTags() {
+        return this.tags;
+    }
+
+    /**
+     * Returns date taken
+     * @return data of photo
+     */
+    public Date getDateTaken() {
+        return this.dateTaken;
+    }
+
+    /**
+     * Sets caption
+     * @param caption new caption
+     */
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    /**
+     * Deletes tag
+     * @param key key of tag to delete
+     */
+    public void deleteTag(String key) {
+        if(tags.containsKey(key)) {
+            tags.remove(key);
+        } else {
+
+        }
+    }
+
+    /**
+     * Deletes tag value
+     * @param key key associated with value
+     * @param value value to delete
+     */
+    public void deleteTagValue(String key, String value) {
+        if(tags.containsKey(key) && tags.get(key).contains(value)) {
+            tags.get(key).remove(value);
+        } else {
+
+        }
+    }
+
+    /**
+     * Adds new tag key
+     * @param key new tag key
+     */
+    public void addTag(String key) {
+        if(tags.containsKey(key)) {
+
+        } else {
+            tags.put(key, new HashSet<String>());
+        }
+    }
+
+    /**
+     * adds new tag
+     * @param key key to add value to
+     * @param value value to add
+     */
+    public void addTag(String key, String value) {
+        Set<String> values = tags.getOrDefault(key, new HashSet<String>());
+        values.add(value);
+        tags.put(key, values);
+    }
+
+}
